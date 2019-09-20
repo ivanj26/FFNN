a4f3e75b8e3dafc8488129be2e4f001ce3f00664
ivanj26 <ivanjth26@gmail.com>
2019-09-20 12:33:05 +0700

    Fix import name

--- testsigmoid.py
+++ testsigmoid.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from util.ActivationFunc import sigmoid
+from util.activation_fun import sigmoid
 
 # create input with range x1 <= x <= x2 with n data
 x = np.linspace(-10, 10, 100)
