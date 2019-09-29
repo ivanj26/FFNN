cfd2140cb1576d936bdab8665d757f7cdab992f1
Andreas Halim <hioe.andre@gmail.com>
2019-09-29 19:08:33 +0700

    add binary accuracy score

--- model/optimizer/minibatchgd.py
+++ model/optimizer/minibatchgd.py
@@ -55,6 +55,28 @@ class MiniBatchGD:
       outputs.append(output)
     return outputs
 
+    def binary_accuracy_score(y_true, y_pred):
+      tp = 0
+      tn = 0
+      fn = 0
+      fp = 0
+      for y_t, y_p in zip(y_true, y_pred):
+        if y_true == round(y_pred) :
+          if y_true == 0:
+            tn += 1
+          else:
+            tp += 1
+        else:
+          if y_true == 0:
+            fn += 1
+          else:
+            fp += 1
+      return (tp + tn) / (tp + tn + fp +fn)
+
+
+
+
+
   def create_mini_batches(self, x, y, batch_size): 
     mini_batches = [] 
     data = np.hstack((x, y)) 
