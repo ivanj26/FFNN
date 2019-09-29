ed6388f8a189dca329b818f3b05a6705e907174e
ivanj26 <ivanjth26@gmail.com>
2019-09-29 11:25:14 +0700

    use data weather

--- model/optimizer/minibatchgd.py
+++ model/optimizer/minibatchgd.py
@@ -22,30 +22,24 @@ class MiniBatchGD:
           output = self.layers[0].feed_forward(x_i)
           for i in range(1, len(self.layers)):
             output = self.layers[i].feed_forward(output)
-          print("output",output)
+
           # calculate mean_squared_error
           error = 0.5 * (output[0] - y_i) * (output[0] - y_i)
-          print("error", error)
           self.error_list.append(error)
+
           # back propagate
           gradients = [y_i - output[0]]
           weights = [[1]]
           for i in range(len(self.layers) - 1, -1, -1):
             weights, gradients = self.layers[i].back_propagate(weights, gradients)
-          print("gradient")
-          for i in range(len(self.layers)):
-            for j in range(len(self.layers[i].neurons)):
-              print(self.layers[i].neurons[j].gradient)
+
           # update delta weights and bias
           update_output = self.layers[0].update_delta_weights(learning_rate=self.lr, momentum=self.momentum, x_before=x_i)
           update_bias = self.layers[0].update_delta_bias(learning_rate=self.lr, momentum=self.momentum)
           for i in range(1, len(self.layers)):
             update_output = self.layers[i].update_delta_weights(learning_rate=self.lr, momentum=self.momentum, x_before=update_output)
             update_bias = self.layers[i].update_delta_bias(learning_rate=self.lr, momentum=self.momentum)
-          print("delta weights and bias")
-          for i in range(len(self.layers)):
-            for j in range(len(self.layers[i].neurons)):
-              print(self.layers[i].neurons[j].delta_weights, self.layers[i].neurons[j].delta_bias)
+
         # update weights and bias
         for i in range(len(self.layers)):
           for j in range(len(self.layers[i].neurons)):
--- testmodel.py
+++ testmodel.py
@@ -9,9 +9,9 @@ import numpy as np
 
 x = [[0.1, 0.9]]
 y = [[0.9]]
-epochs = 100
+epochs = 1000
 
-# X, y = preprocess('./test/data_weather.csv')
+X, y = preprocess('./test/data_weather.csv')
 
 model = Sequential([
   Layer(2),
@@ -21,12 +21,11 @@ model = Sequential([
   Layer(1),
 ])
 
-model.compile()
 model.fit(
-  x,
+  X,
   y,
   epochs=epochs,
-  batch_size=2
+  batch_size=1
 )
 
 # n11 = Node(sigmoid, [-0.2, 0.1], 0.1)
@@ -64,4 +63,5 @@ times = [i for i in range(len(model.optimizer.error_list))]
 plt.plot(times, model.optimizer.error_list)
 plt.show()
 
-print('\n\noutput predict {}: {}'.format(x[0], model.predict([x[0]])))
+print('\n\nerror: {}'.format(model.optimizer.error_list.pop()))
+print('output predict {}: {}'.format(X[0], model.predict([X[0]])))
